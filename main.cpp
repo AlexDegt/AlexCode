bc9a98835539970f479250a721433ba1f11d8399
AlexDegt <degtyarev.aa@phystech.edu>
2017-02-23 00:55:31 +0300

    Version without dump

--- main.cpp
+++ main.cpp
@@ -7,12 +7,8 @@ using namespace std;
 class stack
 {
     public:
-        stack()
-        {
-            sizeVal = 0;
-            capacityVal = 0;
-            data = NULL;
-        }
+        stack();
+        stack(size_t capacit);
         void push(double value);
         double pop();
         void empty();
@@ -21,58 +17,66 @@ class stack
         double * data;
         size_t sizeVal;
         size_t capacityVal;
-        int checkCap(int value);
+        int checkCap();
+        bool ok() const;
 };
 
 int main()
 {
-    stack s;
+    stack s(0);
     s.push(1);
-    //s.push(2);
-    //cout << s.pop() << endl;
-    cout << s.size() << endl;
+    s.push(2);
+    cout << s.pop() << endl;
     system("PAUSE");
     return 0;
 }
+bool stack::ok() const
+{
+    return ((data && capacityVal && sizeVal <= capacityVal) || (!data && !capacityVal && !sizeVal));
+}
+
+stack::stack(size_t capacit)
+    {
+        data = new double[capacit];
+        capacityVal = capacit;
+        sizeVal = 0;
+    }
 
 size_t stack::size()
 {
-    return sizeVal;
+    if (ok())
+    {
+        return sizeVal;
+    }
 }
 
-void stack::push(int value)
+void stack::push(double value)
 {
-    printf("%lld", capacityVal);
-    if (checkCap(value))
+    if (data)
     {
-        if (data)
-            data = (double*) realloc(data,(capacityVal+value)*sizeof(double));
-        else
-            data = (double*) malloc((capacityVal+value)*sizeof(double));
+        if (sizeVal >= capacityVal)
+            data = (double*) realloc(data,(++capacityVal)*sizeof(double));
+        data[sizeVal++] = value;
     }
-    data[sizeVal++] = value;
-    capacityVal++;
+    else
+        cout << "ERROR" << endl;
 }
 
 double stack::pop()
 {
-    return data[sizeVal--];
-}
-
-void stack::empty()    //если мы хотим удалить стэк, то free(this)
-{
-    for (int i = 0; i < sizeVal; i++)
-        data[i] = 0;
-    sizeVal = 0;
-}
-
-int stack::checkCap(int value)
-{
-    if (sizeVal+value-1>capacityVal)
+    if (ok())
     {
-        return 1;
+        return data[sizeVal-1];
+        sizeVal--;
     }
-    else return 0;
 }
 
-//kjbkjhjkhjk
+void stack::empty()
+{
+    if (ok())
+    {
+        for (size_t i = 0; i < sizeVal; i++)
+            data[i] = 0;
+        sizeVal = 0;
+    }
+}
